daf46fa21a5ef2c74788293dd2eca1d67cc5aeaf
ouyangkang <542124953@qq.com>
2019-05-28 17:49:16 +0800

    Initial commit

---
+++ src/main/java/com/krloy/thinkinjava/ThinkinjavaApplication.java
@@ -0,0 +1,13 @@
+package com.krloy.thinkinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkinjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkinjavaApplication.class, args);
+    }
+
+}
