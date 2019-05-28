eb4cfda2d2f8bb833b8311040b6e09f1e0946d16
ouyangkang <542124953@qq.com>
2019-05-28 17:58:10 +0800

    think in java 雏形

---
+++ src/main/java/com/krloy/thinkinjava/第一章/Main.java
@@ -0,0 +1,13 @@
+package com.krloy.thinkinjava.第一章;
+
+/**
+ * @ClassName Main
+ * @Description TODO
+ * @Author ouyangkang
+ * @Date 2019-05-28 17:53
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
